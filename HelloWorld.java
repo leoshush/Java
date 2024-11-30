99b37edbd5483d77533b52fd5607fc084ddc49c6
leoshush <135488598+leoshush@users.noreply.github.com>
2024-11-30 15:57:31 +0000

    datatypes

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String args[])
+    {
+        // Datatypes
+        String ashish  = new String("Ashish");
+        int salary = 250000;
+        char myLetter = 'A';
+        boolean bool = true;
+        // final word is used as if you dont want to overwrite over a variable
+        System.out.println(ashish);
+        System.out.println(salary);
+    }
+
+}
